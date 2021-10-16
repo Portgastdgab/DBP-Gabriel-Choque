41121ca30b85ebd8ff0250c45e6be7ae46488f68
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-10-16 15:31:48 -0500

    end login

--- Python_flask/CV Flask/application.py
+++ Python_flask/CV Flask/application.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
+
+def login():
+    return render_template("login.html")
+
+@app.route("/index")
 def index():
     return render_template("index.html")
 
 @app.route("/hello", methods=["POST"])
+
 def hello():
     name = request.form.get("name")
     lastname = request.form.get("lastname")
